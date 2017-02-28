1bf44cd297908004d87ae52b089a4e06867e179a
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-02-28 20:36:10 +0100

    types a zip code and waits for autocomplete widget

--- src/scripts/foncia-rent-offers.js
+++ src/scripts/foncia-rent-offers.js
@@ -1,22 +1,38 @@
-const casper = require('casper').create();
+const casper = require('casper').create({
+  onError: function(casper, msg, stackTrace) {
+
+    console.log('error:', msg);
+    console.trace(stackTrace);
+  }
+});
 
 const searchCriteria = JSON.parse(casper.cli.options['search-criteria']);
 const searchEngine = JSON.parse(casper.cli.options['search-engine']);
 
 casper.echo('search criteria');
 casper.echo(JSON.stringify(searchCriteria, null, 2));
-
 casper.echo('search engine');
 casper.echo(JSON.stringify(searchEngine, null, 2));
-
-casper.start('http://casperjs.org/');
-
+casper.start(searchEngine.websiteUrl);
+casper.thenClick('.Btn--location');
 casper.then(function() {
-  this.echo('First Page: ' + this.getTitle());
-});
 
-casper.thenOpen('http://phantomjs.org', function() {
-  this.echo('Second Page: ' + this.getTitle());
+  this.sendKeys('#searchForm_localisation_tag', searchCriteria.zipCodes[1], { keepFocus: true });
 });
+casper.waitUntilVisible('.ui-autocomplete .ui-menu-item:first-child', function() {
 
+  var value = casper.evaluate(function() {
+
+    // TODO: trigger arrow up and enter key to simulate user interaction
+
+    return document.querySelector('.ui-autocomplete .ui-menu-item:first-child').textContent;
+    // return document.querySelector('#searchForm_localisation').value;
+  });
+
+  console.log('value', value);
+});
+casper.then(function() {
+
+  this.echo(this.getTitle());
+});
 casper.run();
