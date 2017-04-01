10e9cccef2a20f7322b959694979cd445bd822cb
Itadakimas <said.ahemt@gmail.com>
2017-04-02 00:48:49 +0200

    adds unit tests for offers parsing

--- test/century-21-test.js
+++ test/century-21-test.js
@@ -156,4 +156,91 @@ describe('Century21', function() {
       });
     });
   });
+
+  describe('Offers Parsing', function() {
+
+    it('should parse all offers', function (done) {
+
+      this.timeout(60000);
+
+      let urls = [];
+      let offers = [];
+
+      const sc = new SearchCriteria({
+        maxPrice: 900,
+        minSurfaceArea: 10,
+        offerType: Offer.types.RENT,
+        zipCodes: ['75020', '75019', '75018']
+      });
+
+      Promise.all([
+        Century21SearchEngine.findZipCodeAutocomplete(sc.getZipCodes()[0]),
+        Century21SearchEngine.findZipCodeAutocomplete(sc.getZipCodes()[1]),
+        Century21SearchEngine.findZipCodeAutocomplete(sc.getZipCodes()[2])
+      ])
+      .then(function(zipCodes) {
+
+        const se = new Century21SearchEngine();
+
+        const args = [
+          `--offer-types=${JSON.stringify(Offer.types)}`,
+          `--search-criteria=${JSON.stringify(sc)}`,
+          `--search-engine=${JSON.stringify(se)}`,
+          `--zip-codes=${JSON.stringify(zipCodes)}`
+        ];
+
+        const urlsCasperScript = new CasperScript(`${se.getName()}/get-urls`, args);
+
+        urlsCasperScript.addObserver('data', function(message) {
+
+          if (message.type === 'error')
+          {
+            return done(new SearchEngineException(se.getName(), message.data.message, message.data.trace));
+          }
+          if (message.type === 'urls')
+          {
+            urls = urls.concat(message.data);
+          }
+        });
+
+        urlsCasperScript.addObserver('exit', function(code) {
+
+          if (code > 0)
+          {
+            return done(new SearchEngineException(se.getName(), `get-urls casper script exited with code = ${code}`));
+          }
+
+          args.push(`--urls=${JSON.stringify(urls)}`);
+
+          const offersCasperScript = new CasperScript(`${se.getName()}/get-offers`, args);
+
+          offersCasperScript.addObserver('data', function (message) {
+
+            if (message.type === 'error')
+            {
+              return done(new SearchEngineException(se.getName(), message.data.message, message.data.trace));
+            }
+            if (message.type === 'offer')
+            {
+              offers.push(message.data);
+            }
+          });
+
+          offersCasperScript.addObserver('exit', function(code) {
+
+            if (code > 0)
+            {
+              return done(new SearchEngineException(se.getName(), `get-offers casper script exited with code = ${code}`));
+            }
+            assert.strictEqual(offers.length, urls.length);
+            done();
+          });
+        });
+      })
+      .catch(function(error) {
+
+        done(error);
+      });
+    });
+  });
 });
