b3d3eca40c94f30cea41d145e61c0d953e4e6879
Itadakimas <said.ahemt@gmail.com>
2017-04-17 21:32:18 +0200

    refactors CacheException class

--- src/classes/cache-exception.js
+++ src/classes/cache-exception.js
@@ -2,16 +2,6 @@ const AbstractException = require('./abstract-exception');
 
 class CacheException extends AbstractException
 {
-  // TODO: refactor this part using AbstractException.data property
-  constructor(message, cause = null)
-  {
-    super(message);
-    this.cause = cause;
-  }
-  getCause()
-  {
-    return this.cause;
-  }
 }
 
 module.exports = CacheException;
